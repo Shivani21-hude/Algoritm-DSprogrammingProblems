38000b03b67a946223a03a522b8991e58f886e24
Shivani21-hude <shivani21hude@gmail.com>
2022-12-25 22:47:47 +0530

    [Shivani]-Find Prime Number uto 1000

---
+++ Primenumber/Primenumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prime number from 1 to 1000 :\n");
+
+            for (int i = 2; i <= 1000; i++)
+            {
+                bool isPrimeNumber = true;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        isPrimeNumber = false;
+                        break;
+                    }
+
+                }
+                if (isPrimeNumber)
+                {
+
+                    Console.WriteLine(i + "  ");
+                }
+            }
+        }
+    }
+}
