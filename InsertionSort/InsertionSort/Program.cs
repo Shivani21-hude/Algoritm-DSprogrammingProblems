5553e486939e07dedf8865f1bc40a09f54e5174d
Shivani21-hude <shivani21hude@gmail.com>
2022-12-25 22:19:50 +0530

    [Shivani]-Inseryion Sort

---
+++ InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace InsertionSort
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+         
+            int[] arr = { 4, 7, 1, 9, 6 };
+            int i, j, n, temp;
+            Console.WriteLine("Enter array :\n");
+            foreach(int a in arr)
+            {
+                Console.Write(a+" ");
+            }
+            for (i = 1; i < arr.Length; i++)
+            {
+                temp=arr[i];
+                j=i-1;
+                while(j>=0&& temp > arr[j])
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = temp;
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("Sorted array is :\n");
+            foreach(int a in arr)
+            {
+                Console.Write(a+" ");
+            }
+        }
+    }
+}
