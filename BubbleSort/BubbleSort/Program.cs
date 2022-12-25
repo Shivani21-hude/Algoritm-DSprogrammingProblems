e3b37890828af8d227b9704124a17272731ee9a7
Shivani21-hude <shivani21hude@gmail.com>
2022-12-25 22:00:10 +0530

    [shivani]-Bubble Sort

---
+++ BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 7, 6, 9, 4, 3 };
+            int temp;
+            Console.Write("Given Array : \n ");
+            foreach (int a in arr)
+                Console.Write(a + " ");
+            for (int i = 0; i < arr.Length-1 ; i++)
+            {
+                for (int j = 0; j < arr.Length-1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j+1];
+                        arr[j+1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("\n");
+            Console.Write(" Sorted array : \n");
+            foreach (int a in arr)
+                Console.Write(a + " ");
+           // Console.ReadKey();
+
+        }
+    }
+}
